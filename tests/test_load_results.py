9f5506749d9908db8a3482b57f52a36f723d34a8
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-08-09 14:00:52 +0200

    unsure tests uses results folder

--- tests/test_load_results.py
+++ tests/test_load_results.py
@@ -6,7 +6,7 @@ import mteb
 
 def test_load_results():
     """Ensures that files can be loaded using MTEB"""
-    tests_path = Path(__file__).parent / "results"
+    tests_path = Path(__file__).parent.parent / "results"
 
     os.environ["MTEB_CACHE"] = str(tests_path)
 
