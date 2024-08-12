5f9d25c746ed068136659082b3bfc60ba0e46ee0
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-08-12 11:51:55 +0200

    remove outdated comment

--- remove_spaces_from_large_json_files.py
+++ remove_spaces_from_large_json_files.py
@@ -1,10 +1,8 @@
-import os
 import glob
-import sys
 import json
+import os
 
 for file in glob.glob("results/*/*/*.json"):
-    # if the file is greater than 9 MB, compress it with gzip
     if os.path.getsize(file) >= 9.5 * 1024 * 1024:
         print(f"Resizing {file} to have no indentations")
         # read it in as json and write it out with no indent
