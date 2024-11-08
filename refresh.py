ff3321ada33dea85278c178ef00afa4f1b4306ac
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2024-11-08 19:44:59 +0300

    updates from run

--- refresh.py
+++ refresh.py
@@ -5,12 +5,11 @@ import logging
 import re
 from pathlib import Path
 
-from huggingface_hub import HfApi, get_hf_file_metadata, get_safetensors_metadata, hf_hub_download, hf_hub_url
+from huggingface_hub import HfApi, get_hf_file_metadata, hf_hub_download, hf_hub_url
 from huggingface_hub.errors import NotASafetensorsRepoError
 from huggingface_hub.hf_api import ModelInfo
 from huggingface_hub.repocard import metadata_load
-from mteb import get_task, ModelMeta
-from tqdm.autonotebook import tqdm
+from mteb import ModelMeta, get_task
 
 API = HfApi()
 logging.basicConfig(level=logging.INFO)
@@ -33,7 +32,7 @@ def simplify_dataset_name(name: str) -> str:
     return name.replace("MTEB ", "").split()[0]
 
 
-def get_model_parameters_memory(model_info: ModelInfo) -> tuple[int, float]:
+def get_model_parameters_memory(model_info: ModelInfo) -> tuple[int| None, float|None]:
     try:
         safetensors = API.get_safetensors_metadata(model_info.id)
         num_parameters = sum(safetensors.parameter_count.values())
@@ -56,16 +55,17 @@ def get_model_parameters_memory(model_info: ModelInfo) -> tuple[int, float]:
         if "metadata" in size and "total_size" in size["metadata"]:
             return round(size["metadata"]["total_size"] / bytes_per_param), round(size["metadata"]["total_size"] / 1024 ** 3, 2)
     logger.info(f"Could not find the model parameters for {model_info.id}")
+    return None, None
 
 
-def get_dim_seq_size(model: ModelInfo) -> tuple[str, str, int, float]:
+def get_dim_seq_size(model: ModelInfo) -> tuple[str | None, str | None, int, float]:
     siblings = model.siblings or []
     filenames = [sib.rfilename for sib in siblings]
     dim, seq = None, None
     for filename in filenames:
         if re.match(r"\d+_Pooling/config.json", filename):
             st_config_path = hf_hub_download(model.id, filename=filename)
-            dim = json.load(open(st_config_path)).get("word_embedding_dimension", "")
+            dim = json.load(open(st_config_path)).get("word_embedding_dimension", None)
             break
     for filename in filenames:
         if re.match(r"\d+_Dense/config.json", filename):
@@ -75,10 +75,9 @@ def get_dim_seq_size(model: ModelInfo) -> tuple[str, str, int, float]:
         config_path = hf_hub_download(model.id, filename="config.json")
         config = json.load(open(config_path))
         if not dim:
-            dim = config.get("hidden_dim", config.get("hidden_size", config.get("d_model", "")))
-        seq = config.get("n_positions", config.get("max_position_embeddings", config.get("n_ctx", config.get("seq_length", ""))))
-    if dim is None or seq is None:
-        raise Exception(f"Could not find dim or seq for model {model.id}")
+            dim = config.get("hidden_dim", config.get("hidden_size", config.get("d_model", None)))
+        seq = config.get("n_positions", config.get("max_position_embeddings", config.get("n_ctx", config.get("seq_length", None))))
+
     parameters, memory = get_model_parameters_memory(model)
     return dim, seq, parameters, memory
 
@@ -86,10 +85,22 @@ def get_dim_seq_size(model: ModelInfo) -> tuple[str, str, int, float]:
 def create_model_meta(model_info: ModelInfo) -> None:
     readme_path = hf_hub_download(model_info.id, filename="README.md", etag_timeout=30)
     meta = metadata_load(readme_path)
-    dim, seq, parameters, memory = get_dim_seq_size(model_info)
+    dim, seq, parameters, memory = None, None, None, None
+    try:
+        dim, seq, parameters, memory = get_dim_seq_size(model_info)
+    except Exception as e:
+        logger.error(f"Error getting model parameters for {model_info.id}, {e}")
 
     release_date = str(model_info.created_at.date()) if model_info.created_at else ""
     library = [library_mapping[model_info.library_name]] if model_info.library_name in library_mapping else []
+    languages = meta.get("language", [])
+    if not isinstance(languages, list) and isinstance(languages, str):
+        languages = [languages]
+    # yaml transforms norwegian `no` to False
+    for i in range(len(languages)):
+        if languages[i] is False:
+            languages[i] = "no"
+
     model_dict = ModelMeta(
         name=model_info.id,
         revision=model_info.sha,
@@ -100,7 +111,7 @@ def create_model_meta(model_info: ModelInfo) -> None:
         embed_dim=dim,
         max_tokens=seq,
         n_parameters=parameters,
-        languages=meta.get("language", []),
+        languages=languages,
     )
     model_dir = get_model_dir(model_info.id)
     model_meta_path = model_dir / "model_meta.json"
@@ -120,6 +131,10 @@ def parse_readme(model_info: ModelInfo) -> None:
         logger.info(f"Could not find model-index in {model_id}")
         return
     model_index = meta["model-index"][0]
+    model_name_from_readme = model_index["model_name"]
+    if not model_info.id.endswith(model_name_from_readme):
+        logger.warning(f"Model name mismatch: {model_info.id} vs {model_name_from_readme}")
+        return
     results = model_index.get("results", [])
     model_results = {}
     for result in results:
@@ -129,7 +144,7 @@ def parse_readme(model_info: ModelInfo) -> None:
         if dataset_type not in model_results:
             output_dict["dataset_revision"] = dataset.get("revision", "")
             output_dict["task_name"] = simplify_dataset_name(dataset.get("name", ""))
-            output_dict["evaluation_time"] = 0
+            output_dict["evaluation_time"] = -1
             output_dict["mteb_version"] = "0.0.0"
             output_dict["scores"] = {}
         else:
@@ -139,7 +154,7 @@ def parse_readme(model_info: ModelInfo) -> None:
             mteb_task_metadata = mteb_task.metadata
             mteb_task_eval_languages = mteb_task_metadata.eval_langs
         except Exception:
-            logger.info(f"Error getting task for {model_id} {output_dict['task_name']}")
+            logger.warning(f"Error getting task for {model_id} {output_dict['task_name']}")
             continue
         scores_dict = output_dict["scores"]
         current_split = dataset.get("split", "")
@@ -153,7 +168,15 @@ def parse_readme(model_info: ModelInfo) -> None:
 
         main_score_str = "main_score"
         if main_score_str not in cur_split_metrics:
-            cur_split_metrics[main_score_str] = cur_split_metrics.get(mteb_task.metadata.main_score, 0)
+            # sts old have cos_sim_pearson, but in model_meta cosine_spearman is main_score, sum_eval
+            for old_metric, new_metric in zip(["cos_sim_pearson", "cos_sim_spearman"], ["cosine_pearson", "cosine_spearman"]):
+                if old_metric in cur_split_metrics:
+                    cur_split_metrics[main_score_str] = cur_split_metrics[old_metric]
+            if cur_split_metrics.get(main_score_str, None) is None and mteb_task.metadata.main_score not in cur_split_metrics:
+                logger.warning(f"Could not find main score for {model_id} {output_dict['task_name']}")
+                continue
+
+            cur_split_metrics[main_score_str] = cur_split_metrics[mteb_task.metadata.main_score]
         split_metrics = scores_dict.get(current_split, [])
         split_metrics.append(cur_split_metrics)
         scores_dict[current_split] = split_metrics
@@ -167,10 +190,23 @@ def parse_readme(model_info: ModelInfo) -> None:
 
 
 def get_mteb_data() -> None:
-    models = list(API.list_models(filter="mteb", full=True))
-    models = [model for model in models if model.id == "intfloat/multilingual-e5-large"]
+    models = sorted(list(API.list_models(filter="mteb", full=True)), key=lambda x: x.id)
+    # models = [model for model in models if model.id == "intfloat/multilingual-e5-large"]
     for i, model in enumerate(models, start=1):
         logger.info(f"[{i}/{len(models)}] Processing {model.id}")
+        model_path = get_model_dir(model.id)
+        if (model_path / "model_meta.json").exists():
+            logger.info(f"Model meta already exists for {model.id}")
+            continue
+        if model.id.lower().endswith("gguf"):
+            logger.info(f"Skipping {model.id} GGUF model")
+            continue
+        if model.id.startswith("ILKT"):
+            logger.info(f"Skipping {model.id} ILKT model")
+            continue
+        if model.id.startswith("fine-tuned"):
+            logger.info(f"Skipping {model.id} fine-tuned model")
+            continue
         create_model_meta(model)
         parse_readme(model)
 
