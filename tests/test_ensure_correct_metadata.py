43c7443bc2e35a58719a28422c46d770072be12b
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-11-26 17:12:17 +0100

    fix: remove duplicate models
    - Added minor fixes from #56
    - removed all vectoriseai/* models (e.g. vectoriseai/gte-large)
    
    They seems to be duplicates of the original model.

--- tests/test_ensure_correct_metadata.py
+++ tests/test_ensure_correct_metadata.py
@@ -17,7 +17,7 @@ model_rev_pairs = [
 @pytest.mark.parametrize("model_rev_pair", model_rev_pairs)
 def test_model_meta_in_folders(model_rev_pair):
     """
-    Models added after the 26th of November should contain a model_meta.json file
+    Models added after the November 26, 2024 should contain a model_meta.json file
     """
     model_folder, rev_folder = model_rev_pair
 
@@ -37,14 +37,14 @@ revision_exceptions = [
     ("Snowflake__snowflake-arctic-embed-m-v1.5", "no_revision_available"),
     ("sentence-transformers__all-MiniLM-L12-v2", "no_revision_available"),
     ("nthakur__mcontriever-base-msmarco", "no_revision_available"),
-    ('voyageai__voyage-3-lite', 'no_revision_available')
+    ("voyageai__voyage-3-lite", "no_revision_available"),
 ]
 
 
 @pytest.mark.parametrize("model_rev_pair", model_rev_pairs)
 def test_revision_is_specified_for_new_additions(model_rev_pair):
     """
-    Models added after 26th of November should include a revision ID and can not use the "no_revision_available" fallback.
+    Models added after November 26, 2024 should include a revision ID and can not use the "no_revision_available" fallback.
     """
     model_folder, rev_folder = model_rev_pair
     if (model_folder.name, rev_folder.name) not in revision_exceptions:
@@ -62,7 +62,7 @@ def test_revision_is_specified_for_new_additions(model_rev_pair):
 @pytest.mark.parametrize("model_rev_pair", model_rev_pairs)
 def test_organization_is_specified_for_new_additions(model_rev_pair):
     """
-    Models added after 26th of November should include a organization ID within their name, e.g. "myorg/my_embedding_model".
+    Models added after November 26, 2024 should include a organization ID within their name, e.g. "myorg/my_embedding_model".
 
     This is to avoid mispecified names such as "myorg__my_embedding_model" and similar.
     """
